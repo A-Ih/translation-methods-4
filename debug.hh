ea305a5994e208162dd2c3280fcd4d04f08b2913
Ahmad I <iahmd01@gmail.com>
2023-03-24 21:09:24 +0300

    optimized EXPECT macro

--- debug.hh
+++ debug.hh
@@ -6,22 +6,33 @@
 #include <absl/debugging/stacktrace.h>
 #include <absl/debugging/symbolize.h>
 
+namespace detail {
+
+inline std::string GetStackTrace() {
+    constexpr auto SIZE = 10;
+    void* result[SIZE];
+    int howMuch = absl::GetStackTrace(result, SIZE, 1);
+    std::string trace{"\n"};
+    char buf[1024];
+    for (int i = howMuch - 1; i >= 0; i--) {
+      const char* symbol = "(unknown)";
+      if (absl::Symbolize(result[i], buf, sizeof(buf))) {
+        symbol = buf;
+      }
+      trace += symbol;
+      trace += "\n";
+    }
+    return trace;
+}
+
+}  // namespace detail
+
 // This is made as macro so the message expression won't be computed when not
-// needed (which should be almost always the case)
+// needed (which should be almost always the case).
+// The calculation of stack trace is placed in an external function so that the
+// variables needed for it don't waste space in stack frame of the calling
+// function
 #define EXPECT(cond, message) \
   if (!(cond)) { \
-    constexpr auto SIZE = 10; \
-    std::vector<void*> result{SIZE}; \
-    int howMuch = absl::GetStackTrace(result.data(), SIZE, 0); \
-    std::string trace{"\n"}; \
-    std::vector<char> buf(1024); \
-    for (int i = howMuch - 1; i >= 0; i--) { \
-      const char* symbol = "(unknown)"; \
-      if (absl::Symbolize(result[i], buf.data(), buf.size())) { \
-        symbol = buf.data(); \
-      } \
-      trace += symbol; \
-      trace += "\n"; \
-    } \
-    throw std::runtime_error(trace + absl::StrFormat(__FILE__ ":%d %s", __LINE__, message)); \
+    throw std::runtime_error(detail::GetStackTrace() + absl::StrFormat(__FILE__ ":%d %s", __LINE__, message)); \
   }
