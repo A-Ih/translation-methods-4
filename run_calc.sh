479bd8c14ee5823b35744d089915ab1060244ed4
Ahmad I <iahmd01@gmail.com>
2023-03-27 00:37:41 +0300

    added samples

--- run_calc.sh
+++ run_calc.sh
@@ -9,4 +9,19 @@ fi
 
 ./debug/generator --grammar_file calculator/grammar --out_dir calculator/
 c++ calculator/main.cc -o calculator/out -std=c++17
+
+while read -r line
+do
+    echo "Testing '$line'..."
+    if echo "$line" | ./calculator/out >/dev/null; then
+        echo "OK: parser returned 0"
+    else
+        echo "FAIL: parser failed"
+        exit 1
+    fi
+done <./calculator/samples
+
+
+echo "Now you can enter a custom expression to parse, I will print the result and draw the AST"
+echo "Waiting for your input..."
 ./calculator/out >pic.dot && dot -Tpng pic.dot >pic.png && open pic.png
